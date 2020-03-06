a96a74623b96245c8e6e557c5559bf8fc898aebf
hppRC <hpp.ricecake@gmail.com>
2020-03-07 02:21:14 +0900

    :sparkles: add image

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,15 @@
 import { render } from 'ink';
+import path from 'path';
 import React from 'react';
+import termImg from 'term-img';
+import terminalImage from 'terminal-image';
 
 import UI from './ui';
 
-render(<UI />);
+const fallback = async () => {
+  const image = await terminalImage.file(path.join(__dirname, 'icon.png'));
+  console.log(image);
+  render(<UI />);
+};
+
+termImg(path.join(__dirname, 'icon.png'), { fallback });
